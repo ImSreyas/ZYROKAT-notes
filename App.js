4a12f6b4f47650248e83c1e85f5ca864a51da6b6
ImSreyas <sreyassatheesh755@gmail.com>
2023-02-12 21:42:05 +0530

    started with some basics

--- App.js
+++ App.js
@@ -1,25 +1,54 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar, StyleSheet, Text, View, Image, SafeAreaView } from 'react-native';
+
 
 export default function App() {
   return (
-    <View>
+    <SafeAreaView>
       <View>
-        <StatusBar backgroundColor={ '#FFF' } animated={ true }/>
-      </View>
-      <View style={styles.container}>
-        <View style={styles.card}>
-          <Text>good morning</Text>
+        <View>
+          <StatusBar backgroundColor={ color.primaryBackground } barStyle='dark-content'></StatusBar>
         </View>
-        <View style={styles.card} backgroundColor={"#0f111a"}>
-          <Text>good night</Text>
+
+        <View style= { styles.mainBody }>
+            <View style={ styles.topBar }>
+              <View style={ styles.profileIconCircle }></View>
+            </View>
         </View>
       </View>
-    </View>
+    </SafeAreaView>
   );
 }
 
+const color = {
+  primary: '#658864',
+  secondary: '#B7B78A',
+  primaryBackground: '#EEEEEE',
+  secondaryBackground: '#DDDDDD'
+}
 const styles = StyleSheet.create({
+  mainBody: {
+    width: '100%',
+    height: '100%',
+    backgroundColor: color.primaryBackground,
+    paddingHorizontal: '6%',
+    paddingVertical: '2%'
+  },
+  topBar: {
+    width: '100%',
+    height: '7%',
+    backgroundColor: color.secondaryBackground,
+    borderRadius: 16,
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+    alignItems: 'center'
+  },
+  profileIconCircle: {
+    width: 40,
+    aspectRatio: 1/1,
+    backgroundColor: '#bbb',
+    borderRadius: 1000,
+    marginRight: 16
+  },
   container: {
     width: '100%',
     height: '100%',
@@ -30,6 +59,9 @@ const styles = StyleSheet.create({
   card: {
     width: '80%',
     height: '50%',
-    backgroundColor: '#0f111a'
+    margin: '2%',
+    backgroundColor: '#aaa'
   }
 });
+
+
