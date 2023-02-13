d476f9366217846c728c7d45ccd5f13232039acc
ImSreyas <sreyassatheesh755@gmail.com>
2023-02-14 00:18:19 +0530

    some basic componets are implemented

--- App.js
+++ App.js
@@ -1,67 +1,58 @@
-import { StatusBar, StyleSheet, Text, View, Image, SafeAreaView } from 'react-native';
-
-
+import { StatusBar, Text, View, Image, SafeAreaView, TouchableOpacity, FlatList } from 'react-native';
+import styles from './src/css/homeStyle';
+import main from './src/css/main';
+import color from './src/color/colorPalette';
 export default function App() {
   return (
     <SafeAreaView>
       <View>
         <View>
-          <StatusBar backgroundColor={ color.primaryBackground } barStyle='dark-content'></StatusBar>
+          <StatusBar 
+            backgroundColor={ 
+              color.secondaryBackground 
+            } 
+            barStyle='dark-content'
+          ></StatusBar>
         </View>
-
         <View style= { styles.mainBody }>
             <View style={ styles.topBar }>
+              <View style={ styles.optionImage }>
+                <Image source={ require('./assets/menu-burger.png') } style={ main.icon }></Image>
+              </View>
               <View style={ styles.profileIconCircle }></View>
             </View>
+            <View>
+              <FlatList
+              data={[
+                {key: 'Devin'},
+                {key: 'Dan'},
+                {key: 'Dominic'},
+                {key: 'Jackson'},
+                {key: 'James'},
+                {key: 'Joel'},
+                {key: 'John'},
+                {key: 'Jillian'},
+                {key: 'Jimmy'},
+                {key: 'Julie'},
+              ]}
+              renderItem={({item}) => <View style={styles.item}></View>}
+              numColumns={2}
+              style={ styles.fl }
+            />
+            </View>
+        </View>
+        <View style={ main.navigationBar }>
+          <View style= { styles.addButtonContainer }>
+            <TouchableOpacity title='' style={ styles.addButton }>
+              <Image source={ require('./assets/plus.png') } style={ main.icon }></Image>
+            </TouchableOpacity>
+          </View>
         </View>
       </View>
     </SafeAreaView>
   );
 }
 
-const color = {
-  primary: '#658864',
-  secondary: '#B7B78A',
-  primaryBackground: '#EEEEEE',
-  secondaryBackground: '#DDDDDD'
-}
-const styles = StyleSheet.create({
-  mainBody: {
-    width: '100%',
-    height: '100%',
-    backgroundColor: color.primaryBackground,
-    paddingHorizontal: '6%',
-    paddingVertical: '2%'
-  },
-  topBar: {
-    width: '100%',
-    height: '7%',
-    backgroundColor: color.secondaryBackground,
-    borderRadius: 16,
-    flexDirection: 'row',
-    justifyContent: 'flex-end',
-    alignItems: 'center'
-  },
-  profileIconCircle: {
-    width: 40,
-    aspectRatio: 1/1,
-    backgroundColor: '#bbb',
-    borderRadius: 1000,
-    marginRight: 16
-  },
-  container: {
-    width: '100%',
-    height: '100%',
-    backgroundColor: "#FFF",
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  card: {
-    width: '80%',
-    height: '50%',
-    margin: '2%',
-    backgroundColor: '#aaa'
-  }
-});
+
 
 
